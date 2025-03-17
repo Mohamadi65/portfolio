5562cc566bb063e852648b4b649ce3d56e0739f9
Mohamadi65 <freelancemohamadi65@gmail.com>
2025-03-17 19:40:36 +0100

    Suppression de la transition sur Details

--- app/_components/Details.js
+++ app/_components/Details.js
@@ -2,14 +2,12 @@ import React from "react";
 
 export default function Details({ children }) {
   return (
-    <div className="fixed inset-0 z-[1001] min-h-screen w-screen backdrop-blur-xl overflow-y-auto ">
-    {/* Arrière-plan flou */}
-    {/* <div className="absolute inset-0 bg-slate-900/10 backdrop-blur-sm "></div> */}
-  
-    {/* Contenu net */}
-    <div className="relative z-[1002]   px-5 py-2  ">
-      {children}
+    <div className="fixed inset-0 z-[1001] min-h-screen  w-screen backdrop-blur-xl overflow-y-auto ">
+      {/* Arrière-plan flou */}
+      {/* <div className="absolute inset-0 bg-slate-900/10 backdrop-blur-sm "></div> */}
+
+      {/* Contenu net */}
+      <div className="relative z-[1002]   px-5 py-2  ">{children}</div>
     </div>
-  </div>
   );
 }
